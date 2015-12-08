fb4e07393b71bb937d256a4229ed4d1b8c3b4cc2
Paul Bereza <paulbereza@gmail.com>
2015-12-08 03:30:19 +0000

    day6 solution

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,46 +1,58 @@
+
+extern crate regex;
+
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::env;
+use std::cmp::max;
+use regex::Regex;
 
-fn toggle(board : &mut [[bool; 1000]; 1000], x0: usize, x1: usize, y0: usize, y1:usize) {
+fn toggle(val : i32) -> i32 {
+    val + 2
+}
+
+fn turn_on(val : i32) -> i32 {
+    val + 1
+}
+
+fn turn_off(val : i32) -> i32 {
+    max(val - 1, 0)
+}
+
+
+fn handle_line(board : &mut [[i32; 1000]; 1000], line : String){
+    let re = Regex::new("(turn on|toggle|turn off) (\\d+),(\\d+) through (\\d+),(\\d+)").unwrap();
+
+    let captures = re.captures(&line).unwrap();
+
+    let f = match(captures.at(1)) {
+        Some("turn on") => turn_on as fn(i32) -> i32,
+        Some("turn off") => turn_off as fn(i32) -> i32,
+        Some("toggle") => toggle as fn(i32) -> i32,
+        _ => panic!("unmatched line {}", line),
+    };
+
+    let x0 = captures.at(2).unwrap().parse::<usize>().unwrap();
+    let y0 = captures.at(3).unwrap().parse::<usize>().unwrap();
+    let x1 = captures.at(4).unwrap().parse::<usize>().unwrap() + 1;
+    let y1 = captures.at(5).unwrap().parse::<usize>().unwrap() + 1;
+
     for x in x0..x1 {
         for y in y0..y1 {
-            board[x][y] = !board[x][y];
+            board[x][y] = f(board[x][y]);
         }
     }
 }
 
-fn turn_on(board : &mut [[bool; 1000]; 1000], x0: usize, x1: usize, y0: usize, y1:usize) {
-    for x in x0..x1 {
-        for y in y0..y1 {
-            board[x][y] = true;
-        }
-    }
-}
-
-fn turn_off(board : &mut [[bool; 1000]; 1000], x0: usize, x1: usize, y0: usize, y1:usize) {
-    for x in x0..x1 {
-        for y in y0..y1 {
-            board[x][y] = false;
-        }
-    }
-}
-
-
-fn handle_line(board : &mut [[bool; 1000]; 1000], line : String){
-}
-
-fn count_board(board : [[bool; 1000]; 1000]) -> i32 {
+fn count_board(board : &[[i32; 1000]; 1000]) -> i32 {
     let mut c = 0;
     for x in 0..1000 {
         for y in 0..1000 {
-            if board[x][y] {
-                c+=1;
-            }
+            c+=board[x][y];
         }
     }
-    return c;
+    c
 }
 
 fn main() {
@@ -55,14 +67,14 @@ fn main() {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(&file);
 
-    let mut board = [[false; 1000]; 1000];
+    let mut board = [[0; 1000]; 1000];
 
     for line in reader.lines() {
         handle_line(&mut board, line.unwrap());
     }
 
 
-    println!("lights on = {}", count_board(board));
+    println!("lights on = {}", count_board(&board));
 
 }
 
